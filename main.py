a29cef51988fbfb35795a2f72137c99f7d7e5c19
Frederik Heda <frederik.heda@gmx.net>
2021-01-17 18:50:48 +0000

    Update Maif file (update description)

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 ## Object Detection File
 
+## MEng Mechanical and Electrical Engineering
+## EN4604 Individual Project
+##Frederik Heda
+
+
+#Import Necessary Libraries
 import os
 import cv2
 import numpy as np
@@ -12,5 +18,3 @@ import sys
 # Set up camera constants
 IM_WIDTH = 1280
 IM_HEIGHT = 720
-
-#test
