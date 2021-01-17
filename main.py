c2484449ba671c24d0c87c0fe3a751f5af4bb355
Frederik Heda <frederik.heda@gmx.net>
2021-01-17 17:55:34 +0000

    Update main file (Import libraries)

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 ## Object Detection File
+
+import os
+import cv2
+import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import tensorflow as tf
+import argparse
+import sys
+
+# Set up camera constants
+IM_WIDTH = 1280
+IM_HEIGHT = 720
